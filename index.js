464f9c400445cffa9b04ec006aba792743edc54e
mikach <mikach27@gmail.com>
2014-04-29 20:37:30 +0300

    Add promises support

--- index.js
+++ index.js
@@ -1,15 +1,14 @@
 'use strict';
 
+var aster = require('aster');
 var uglify = require('uglify-js');
 var parse = require('esprima').parse;
 
 module.exports = function(options) {
     var compressor = uglify.Compressor(options);
-    return function(ast) {
-        return ast.map(function(syntax) {
-            var uglify_ast = uglify.AST_Node.from_mozilla_ast(syntax);
-            uglify_ast.figure_out_scope();
-            return parse( uglify_ast.transform(compressor).print_to_string() );
-        });
-    };
+    return aster.transform.mapper(function(syntax) {
+        var uglify_ast = uglify.AST_Node.from_mozilla_ast(syntax);
+        uglify_ast.figure_out_scope();
+        return parse( uglify_ast.transform(compressor).print_to_string() );
+    });
 };
--- test.js
+++ test.js
@@ -1,11 +1,15 @@
 var expect = require('chai').expect;
 var asterUglify = require('./index.js');
 var parse = require('esprima').parse;
+var Promise = require('bluebird');
 
 describe('aster-uglify', function() {
-    it('should works with array of syntax-trees', function() {
+    it('should works with array of syntax-trees', function(done) {
         var sources = ['var aaa = 1; if (true) { console.log(aaa); }', 'if(false) { } else { var bbb = 2; }'].map(parse);
         var expected = ['var aaa=1; console.log(aaa);', 'var bbb = 2;'].map(parse);
-        expect( asterUglify()(sources) ).to.eql(expected);
+        Promise.all(asterUglify()(sources)).then(function(res) {
+            expect( res ).to.eql(expected);
+            done();
+        });
     });
 });
