a8c6759e0e9e65650b24d9d5dc1135603e3f4595
mikach <mikach27@gmail.com>
2014-04-29 19:40:47 +0300

    Add test

---
+++ test.js
@@ -0,0 +1,11 @@
+var expect = require('chai').expect;
+var asterUglify = require('./index.js');
+var parse = require('esprima').parse;
+
+describe('aster-uglify', function() {
+    it('should works with array of syntax-trees', function() {
+        var sources = ['var aaa = 1; if (true) { console.log(aaa); }', 'if(false) { } else { var bbb = 2; }'].map(parse);
+        var expected = ['var aaa=1; console.log(aaa);', 'var bbb = 2;'].map(parse);
+        expect( asterUglify(sources) ).to.eql(expected);
+    });
+});
