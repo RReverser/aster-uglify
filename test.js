d092cf525725a34ab449632efc9386930301bceb
Ingvar Stepanyan <me@rreverser.com>
2014-05-13 01:47:10 +0300

    Added more UglifyJS options; souce map support.

--- test.js
+++ test.js
@@ -4,7 +4,7 @@ var parse = require('esprima').parse;
 var asterUglify = require('./index.js');
 
 describe('aster-uglify', function() {
-    it('should works with array of syntax-trees', function(done) {
+    it('should work with array of syntax-trees', function(done) {
         var sources = ['var a = 1; if (true) { console.log(a); }', 'if(false) { } else { var b = 2; }'].map(parse);
         var expected = ['var a=1; console.log(a);', 'var b = 2;'].map(parse);
         Promise.all(asterUglify({ warnings: false })(sources)).then(function(res) {
