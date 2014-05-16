4f7a47dbe9edd56f754c9a17c49a7833d25974d7
mikach <mikach27@gmail.com>
2014-05-16 16:49:03 +0300

    Update tests

--- test/test.js
+++ test/test.js
@@ -1,15 +1,24 @@
 var expect = require('chai').expect;
 var aster = require('aster');
-var Promise = require('aster').Promise;
+var Promise = aster.Promise;
 var parse = require('esprima').parse;
 var asterUglify = require('../');
-
 var fs = require('fs');
 
 describe('aster-uglify', function() {
     it('should work with array of syntax-trees', function() {
-        return aster.src('test/ex*.js')
+        var expected, actual;
+        return aster.src('./test/from*.js')
             .then( asterUglify({ compress: { warnings: false } }) )
-            .then(aster.dest('tmp'));
+            .then(aster.dest('tmp'))
+            .then(function() {
+                var ast1 = parse( fs.readFileSync('./tmp/test/from1.js') );
+                var exp_ast1 = parse( fs.readFileSync('./test/to1.js') );
+                expect(ast1).to.eql(exp_ast1);
+
+                var ast2 = parse( fs.readFileSync('./tmp/test/from2.js') );
+                var exp_ast2 = parse( fs.readFileSync('./test/to2.js') );
+                expect(ast2).to.eql(exp_ast2);
+            });
     });
 });
